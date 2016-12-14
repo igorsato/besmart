eec66997118c34c3c66b60651bca703f683fcd1d
igor <igor.sato@outlook.com>
2016-12-14 00:27:06 -0200

    Alteração do DB

--- besmart/db/schema.rb
+++ besmart/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161204174511) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accesses", force: :cascade do |t|
     t.string   "name"
     t.boolean  "active",     default: true
@@ -35,8 +38,8 @@ ActiveRecord::Schema.define(version: 20161204174511) do
     t.boolean  "main_address", default: false
     t.string   "alias"
     t.integer  "state_id"
-    t.index ["customer_id"], name: "index_addresses_on_customer_id"
-    t.index ["state_id"], name: "index_addresses_on_state_id"
+    t.index ["customer_id"], name: "index_addresses_on_customer_id", using: :btree
+    t.index ["state_id"], name: "index_addresses_on_state_id", using: :btree
   end
 
   create_table "categories", force: :cascade do |t|
@@ -49,8 +52,8 @@ ActiveRecord::Schema.define(version: 20161204174511) do
   create_table "categories_has_products", id: false, force: :cascade do |t|
     t.integer "product_id"
     t.integer "category_id"
-    t.index ["category_id"], name: "index_categories_has_products_on_category_id"
-    t.index ["product_id"], name: "index_categories_has_products_on_product_id"
+    t.index ["category_id"], name: "index_categories_has_products_on_category_id", using: :btree
+    t.index ["product_id"], name: "index_categories_has_products_on_product_id", using: :btree
   end
 
   create_table "customers", force: :cascade do |t|
@@ -72,9 +75,9 @@ ActiveRecord::Schema.define(version: 20161204174511) do
     t.string   "last_sign_in_ip"
     t.integer  "access_id"
     t.boolean  "active"
-    t.index ["access_id"], name: "index_customers_on_access_id"
-    t.index ["email"], name: "index_customers_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+    t.index ["access_id"], name: "index_customers_on_access_id", using: :btree
+    t.index ["email"], name: "index_customers_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "galleries", force: :cascade do |t|
@@ -88,7 +91,7 @@ ActiveRecord::Schema.define(version: 20161204174511) do
     t.boolean  "active"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
-    t.index ["attachable_type", "attachable_id"], name: "index_galleries_on_attachable_type_and_attachable_id"
+    t.index ["attachable_type", "attachable_id"], name: "index_galleries_on_attachable_type_and_attachable_id", using: :btree
   end
 
   create_table "newsletters", force: :cascade do |t|
@@ -107,8 +110,8 @@ ActiveRecord::Schema.define(version: 20161204174511) do
     t.decimal  "total_price", precision: 12, scale: 2
     t.datetime "created_at",                                       null: false
     t.datetime "updated_at",                                       null: false
-    t.index ["order_id"], name: "index_order_items_on_order_id"
-    t.index ["product_id"], name: "index_order_items_on_product_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id", using: :btree
+    t.index ["product_id"], name: "index_order_items_on_product_id", using: :btree
   end
 
   create_table "order_statuses", force: :cascade do |t|
@@ -120,20 +123,20 @@ ActiveRecord::Schema.define(version: 20161204174511) do
   create_table "orders", force: :cascade do |t|
     t.integer  "payment_id"
     t.integer  "customer_id"
-    t.datetime "created_at",                                                         null: false
-    t.datetime "updated_at",                                                         null: false
+    t.datetime "created_at",                                               null: false
+    t.datetime "updated_at",                                               null: false
     t.integer  "order_status_id"
-    t.decimal  "subtotal",                  precision: 12, scale: 3, default: "0.0"
-    t.decimal  "tax",                       precision: 12, scale: 3
-    t.decimal  "shipping",                  precision: 12, scale: 3
-    t.decimal  "total",                     precision: 12, scale: 3, default: "0.0"
-    t.integer  "order_number",    limit: 8
+    t.decimal  "subtotal",        precision: 12, scale: 3, default: "0.0"
+    t.decimal  "tax",             precision: 12, scale: 3
+    t.decimal  "shipping",        precision: 12, scale: 3
+    t.decimal  "total",           precision: 12, scale: 3, default: "0.0"
+    t.bigint   "order_number"
     t.integer  "address_id"
     t.string   "shipping_name"
-    t.index ["address_id"], name: "index_orders_on_address_id"
-    t.index ["customer_id"], name: "index_orders_on_customer_id"
-    t.index ["order_status_id"], name: "index_orders_on_order_status_id"
-    t.index ["payment_id"], name: "index_orders_on_payment_id"
+    t.index ["address_id"], name: "index_orders_on_address_id", using: :btree
+    t.index ["customer_id"], name: "index_orders_on_customer_id", using: :btree
+    t.index ["order_status_id"], name: "index_orders_on_order_status_id", using: :btree
+    t.index ["payment_id"], name: "index_orders_on_payment_id", using: :btree
   end
 
   create_table "payments", force: :cascade do |t|
@@ -172,4 +175,13 @@ ActiveRecord::Schema.define(version: 20161204174511) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "addresses", "customers"
+  add_foreign_key "addresses", "states"
+  add_foreign_key "customers", "accesses"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
+  add_foreign_key "orders", "addresses"
+  add_foreign_key "orders", "customers"
+  add_foreign_key "orders", "order_statuses"
+  add_foreign_key "orders", "payments"
 end
